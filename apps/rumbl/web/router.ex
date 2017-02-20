f874ae31c2885472f1da088e1be3274104c26604
iampeterbanjo <peterbanjo@gmail.com>
2017-02-20 06:14:08 +0000

    update users to REST, add model validation and form

--- apps/rumbl/web/router.ex
+++ apps/rumbl/web/router.ex
@@ -20,9 +20,7 @@ defmodule Rumbl.Router do
   scope "/rumbl", Rumbl do
     pipe_through :browser # Use the default browser stack
 
-    get "/users", UserController, :index
-    get "/users/:id", UserController, :show
-    get "/", UserController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
   end
 
   # Other scopes may use custom stacks.
